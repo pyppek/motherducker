9b0a1d16028f0ea02cbaf927c1d11e3c5d1ca384
laancornelis <laancornelis@gmail.com>
2019-12-16 12:46:26 +0100

    created list with fake payloads

--- motherducker/app/views.py
+++ motherducker/app/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+from payloads.models import Payload
 
 
 class HomePageView(TemplateView):
@@ -7,11 +9,13 @@ class HomePageView(TemplateView):
 class ConnectionsView(TemplateView):
     template_name = 'connections.html'
 
-class TerminalView(TemplateView):
-    template_name = 'terminal.html'
-
 class ScriptsView(TemplateView):
     template_name = 'scripts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ScriptsView, self).get_context_data(**kwargs)
+        context['payloads'] = Payload.objects.all()
+        return context
 
 class ConnectionDetailsView(TemplateView):
     template_name = 'connection_details.html'
