5ebe1845a5e74ed93fb14b1f6251ad73cacd20e7
Dilkovak <56413259+Dilkovak@users.noreply.github.com>
2019-12-17 11:13:26 +0100

    Added terminal to page

--- motherducker/app/views.py
+++ motherducker/app/views.py
@@ -1,17 +1,35 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
 
+
 class ConnectionsView(TemplateView):
     template_name = 'connections.html'
+
 
 class TerminalView(TemplateView):
     template_name = 'terminal.html'
 
+    def get(self, request, *args, **kwargs):
+        context = {}
+        # TODO change this so it picks it up through reverse shell which folder user resides in
+        context['terminal'] = 'C:\\>'
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        context = {}
+        # TODO change this so it picks it up through reverse shell which folder user resides in
+        context['terminal'] = 'C:\\>'
+        return render(request, self.template_name, context)
+
+
 class ScriptsView(TemplateView):
     template_name = 'scripts.html'
+
 
 class ConnectionDetailsView(TemplateView):
     template_name = 'connection_details.html'
