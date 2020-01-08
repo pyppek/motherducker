fa5c1d65dc65692c6e59327feff489c0916fa977
Dil Kovak <github@dilkovak.com>
2020-01-08 18:46:53 +0100

    small fix

--- motherducker/motherducker/settings_template.py
+++ motherducker/motherducker/settings_template.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'payloads',
+    'connections',
+    'rest_framework',
+    'django_filter'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'motherducker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
