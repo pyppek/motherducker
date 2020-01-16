a3f56281a9571236ed03f50758e68fb7c9e1aacf
puijjar <puijjar@gmail.com>
2020-01-16 13:08:46 -0500

    Fixed (read: "Whooops, readded") static dirs.

--- motherducker/motherducker/settings.py
+++ motherducker/motherducker/settings.py
@@ -123,4 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/var/www/motherducker'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'), 'static',
+)
+
+STATIC_ROOT = '/var/www/motherducker/static'
