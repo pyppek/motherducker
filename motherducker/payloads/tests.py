b1ab35a8bace3fbae540fdf253aaeb0d95f248ac
Dilkovak <github@dilkovak.com>
2020-01-21 11:33:14 +0000

    added testcase for connections

--- motherducker/payloads/tests.py
+++ motherducker/payloads/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
-
+from connections.models import Connection
+from payloads.models import Payload, TerminalLog, ScriptLog, TerminalHistory
 # Create your tests here.
